65a883a860cd26d6096cbb67cb2fa5511d1ec2f6
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2025-08-05 01:30:47 +0530

    Clean up tracked generated folders

---
+++ learn/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\psy_k\\OneDrive\\Desktop\\RubixGo\\vaishnav\\learnRubix\\learn\\learn\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\psy_k\\OneDrive\\Desktop\\RubixGo\\vaishnav\\learnRubix\\learn\\learn\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\psy_k\\OneDrive\\Desktop\\RubixGo\\vaishnav\\learnRubix\\learn\\learn\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\psy_k\\OneDrive\\Desktop\\RubixGo\\vaishnav\\learnRubix\\learn\\learn\\src\\css\\custom.css"),
+];
